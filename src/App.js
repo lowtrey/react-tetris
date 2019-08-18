e7f271628c2a2397fc15304658048474aa8cd616
Q. Price <priceqp@gmail.com>
2019-08-17 22:05:44 -0500

    Minor refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Tetris from './components/Tetris';
 
-const App = () => (
-  <div className='App'>
-    <Tetris />
-  </div>
-);
+const App = () => <div className='App'><Tetris /></div>;
 
 export default App;
