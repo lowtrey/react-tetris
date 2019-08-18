1f12ab3e63f5c8aca826f8a1463c1541cb80ccd7
Q. Price <priceqp@gmail.com>
2019-08-18 14:02:16 -0500

    Create usePlayer hook

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,8 @@ import { createStage } from '../gameHelpers';
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const Tetris = () => {
+  console.log(createStage());
+  
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
