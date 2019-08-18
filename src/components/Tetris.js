c92caecb648328928d76d90fcb951cde5009ccb8
Q. Price <priceqp@gmail.com>
2019-08-18 17:02:20 -0500

    Fix collision detection for bottom of stage, fix resetPlayer bug

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Display from './Display';
 import Stage from './Stage';
 import StartButton from './StartButton';
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
@@ -12,20 +12,33 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('Re-Render');
 
   const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if(!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    };
   };
   const startGame = () => {
     // Reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if(!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if(player.pos.y < 1) {
+        console.log('GAME OVER!!!');
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
   const dropPlayer = () => {
     drop();
